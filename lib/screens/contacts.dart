20655ed4ae0e151b6e75c1729795fe487625020d
onkar-shaligram <onkarshaligram01@gmail.com>
2020-06-25 13:08:22 +0530

    first commit

---
+++ lib/screens/contacts.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ContactsScreen extends StatefulWidget {
+  @override
+  _ContactsScreenState createState() => _ContactsScreenState();
+}
+
+class _ContactsScreenState extends State<ContactsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
